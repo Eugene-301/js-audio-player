bf8453f10cf9a253b111391f7bffc1631befbcf8
Eugene-301 <ablepiha@duck.com>
2025-05-24 16:40:15 +0300

    back to start

--- main.js
+++ main.js
@@ -1,34 +1,9 @@
 import { app, BrowserWindow, ipcMain } from "electron/main";
 import path from "node:path";
-import fs from "node:fs";
 import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-console.log(path.join(__dirname, "preload.js"));
-
-if (!fs.existsSync(path.join(__dirname, "preload.js"))) {
-  console.error("Файл preload.js не найден по пути:", preloadPath);
-} else {
-  console.log("файл найден");
-}
-
-async function handleSquirrelEvent() {
-  try {
-    const squirrelStartup = await import("electron-squirrel-startup");
-    if (squirrelStartup.default) {
-      app.quit();
-      return true;
-    }
-  } catch (error) {
-    console.error("Failed to load electron-squirrel-startup:", error);
-  }
-  return false;
-}
-
-handleSquirrelEvent().catch((error) => {
-  console.log(error);
-});
 
 const createWindow = () => {
   const win = new BrowserWindow({
