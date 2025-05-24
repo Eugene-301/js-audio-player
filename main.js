0f8255990d2366647fa6cab9309eb5fe6f7c6506
Eugene-301 <ablepiha@duck.com>
2025-05-24 20:24:54 +0300

    Added forge

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 
+if (require("electron-squirrel-startup")) app.quit();
+
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    show: true,
+    center: true,
+    show: false,
+    resizable: false,
     autoHideMenuBar: true,
+    minimizable: false,
+    maximizable: false,
+    fullscreen: false,
+    fullscreenable: false,
+    hasShadow: false,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
   });
+
+  win.setResizable(false);
 
   win.once("ready-to-show", () => {
     win.show();
