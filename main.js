299e04ef3f98daf04a013ffcee19aae006ababb2
Eugene-301 <ablepiha@duck.com>
2025-05-24 12:25:34 +0300

    Upgraded to ES modules

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-const { app, BrowserWindow } = require("electron");
+import { app, BrowserWindow } from "electron";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url); // Получаем путь к текущему файлу
+const __dirname = path.dirname(__filename);
 
 const createWindow = () => {
   const window = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
   window.loadFile("index.html");
